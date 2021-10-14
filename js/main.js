b85cf7e3a43293d9db101625b174c0504a0ec4b6
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-10-14 17:04:48 -0300

    Adicionando o arquivo "js/main.js", que contém as funções 1): "calcular_idade()"; e 2): "mostrar_idade()". 2) recebe os dados do usuário e chama 1) para calcular a idade com base na data do navegador.

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const calcular_idade = (dados_cliente) => {
+    const data = new Date
+    const data_ano = data.getFullYear()
+    const data_mes = data.getMonth()    
+    const data_dia = data.getDate()
+
+    const idade = data_ano - dados_cliente.ano_nascimento
+        if (data_mes > dados_cliente.mes_nascimento) {
+            return idade
+        }
+        else if (data_mes == dados_cliente.mes_nascimento) {
+            if (data_dia >= dados_cliente.dia_nascimento) {
+                return idade
+            }
+        } return idade - 1
+}
+
+const mostrar_idade = () => {
+    let dados_cliente = {
+        nome: document.getElementById("nome").value,
+        dia_nascimento: document.getElementById("dia").value,
+        mes_nascimento: document.getElementById("mes").value,
+        ano_nascimento: document.getElementById("ano").value
+    }
+    console.log(
+        `Meu nome é ${dados_cliente.nome} e tenho `
+        + `${calcular_idade(dados_cliente)} anos de idade.`
+    )
+}
